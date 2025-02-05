cfad18b917440f324122579350edf1f6637ae7fb
Sakura <sakurathetrans@gmail.com>
2025-02-05 16:19:48 -0500

    FINALLY

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
     static ArrayList<String> cities = new ArrayList<>();
     static ArrayList<Double> temps = new ArrayList<>();
-    static Map<String, ArrayList<Double>> cityNTemp = new HashMap<>();
-    static ArrayList<Double> point(ArrayList<Double> val){
-        return val;
+    static Map<String, String> cityNTemp = new HashMap<>();
+
+    // Credit to stack overflow for this one
+    static <K, V> K getKeyByValue(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (entry.getValue().equals(value)) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
+
     static void getCities(){
         String userInput = "";
         while (!userInput.equals("END")){
@@ -27,23 +32,34 @@ public class Main {
         for (String city : cities) {
             temps.clear();
             for (String day : days) {
-                System.out.println("cityNTemp : " + cityNTemp);
                 System.out.println("Please input the temp for " + day + " in " + city);
                 temps.add(input.nextDouble());
             }
-            System.out.println("cityNTemp : " + cityNTemp);
-
-            cityNTemp.put(city, temps);
-
-            System.out.println("temps : " + temps);
-            System.out.println("cityNTemp : " + cityNTemp);
+            final Object[] falseStrArray = temps.toArray();
+            cityNTemp.put(city, Arrays.toString(falseStrArray));
         }
-        System.out.println("cityNTemp : " + cityNTemp);
+    }
+
+    static void getAvg() {
+        for (String value : cityNTemp.values()) {
+            String cityKey = getKeyByValue(cityNTemp, value);
+            value = value.substring(1, value.length() - 1);
+            double avg = 0;
+            String[] tempSet = value.split(", ");
+            for (String s : tempSet) {
+                avg = avg + Double.parseDouble(s);
+            }
+            avg = avg / 5;
+
+            cityNTemp.replace(cityKey, Double.toString(avg));
+            System.out.println(cityKey + " has an average temp of " + cityNTemp.get(cityKey));
+        }
     }
 
     public static void main(String[] args) {
         getCities();
         getTemps();
+        getAvg();
 
     }
 }
