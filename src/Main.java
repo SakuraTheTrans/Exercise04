097062e2412c2e620a419ea4342c1830eb1a7d42
Sakura <sakurathetrans@gmail.com>
2025-02-05 14:31:03 -0500

    PLEASE WORK

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<String> cities = new ArrayList<>();
+    static ArrayList<Double> temps = new ArrayList<>();
+    static Map<String, ArrayList<Double>> cityNTemp = new HashMap<>();
+    static ArrayList<Double> point(ArrayList<Double> val){
+        return val;
+    }
+    static void getCities(){
+        String userInput = "";
+        while (!userInput.equals("END")){
+            System.out.println("Please input a city name, OR input 'END' to continue :");
+            userInput = input.nextLine();
+            if(!userInput.equals("END")) {
+                cities.add(userInput);
+            }
+        }
+    }
+
+    static void getTemps(){
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        for (String city : cities) {
+            temps.clear();
+            for (String day : days) {
+                System.out.println("cityNTemp : " + cityNTemp);
+                System.out.println("Please input the temp for " + day + " in " + city);
+                temps.add(input.nextDouble());
+            }
+            System.out.println("cityNTemp : " + cityNTemp);
+
+            cityNTemp.put(city, temps);
+
+            System.out.println("temps : " + temps);
+            System.out.println("cityNTemp : " + cityNTemp);
+        }
+        System.out.println("cityNTemp : " + cityNTemp);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        getCities();
+        getTemps();
+
     }
 }
